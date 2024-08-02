088d45314405c0ed1d99483d277a7ffbe70aa06c
Urvashi146 <aidev.urvashi@gmail.com>
2024-08-02 17:54:28 +0530

    express first app

---
+++ crud.js
@@ -0,0 +1,29 @@
+
+const express = require("express");
+ const app = express();
+
+//  const port = process.env.PORT || 3000;
+
+
+ app.get("/",(req,res)=> {
+
+    res.send("hi i am alive")
+
+
+
+ })
+
+ const start = async () => {
+
+    try{
+        app.listen(port, () => {
+            console.log(`${port} yes i am connected`)
+        })
+        
+    }
+    catch{
+    
+        console.log("Error==>",error)
+    
+    }
+ }
