d11aefda731d24dfefbc3ff8505c60f2b4043b95
Urvashi146 <aidev.urvashi@gmail.com>
2024-08-05 11:06:23 +0530

    test

--- crud.js
+++ crud.js
@@ -1,29 +1,37 @@
 
-const express = require("express");
- const app = express();
+// const express = require("express");
+//  const app = express();
 
-//  const port = process.env.PORT || 3000;
-
-
- app.get("/",(req,res)=> {
-
-    res.send("hi i am alive")
+// //  const port = process.env.PORT || 3000;
 
 
+//  app.get("/",(req,res)=> {
 
- })
+//     res.send("hi i am alive")
 
- const start = async () => {
 
-    try{
-        app.listen(port, () => {
-            console.log(`${port} yes i am connected`)
-        })
+
+//  })
+
+//  const start = async () => {
+
+//     try{
+//         app.listen(port, () => {
+//             console.log(`${port} yes i am connected`)
+//         })
         
-    }
-    catch{
+//     }
+//     catch{
     
-        console.log("Error==>",error)
+//         console.log("Error==>",error)
     
-    }
- }
+//     }
+//  }
+const express = require('express')
+const app = express()
+
+app.get('/', (req, res) => {
+  res.send('hello world')
+})
+
+app.listen(3000)
